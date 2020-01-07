f185bd0e78f41c7f522df805b5d5fd2f04933c76
Ignacio Iacovino <nacho.iacovino@gmail.com>
2020-01-07 17:07:04 +0100

    Change Redux to Redux Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,39 +1,39 @@
 import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import './App.css'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import { setSearchField, setRequestRobots } from '../redux/actions'
 
-const App = ({ robots, isPending, searchField, onSearchChange, onRequestRobots }) => {
-	useEffect(() => onRequestRobots(), [onRequestRobots])
+const App = () => {
+	const dispatch = useDispatch()
+	
+	const searchField = useSelector(({ searchRobots }) => searchRobots.searchField)
+	const robots = useSelector(({ requestRobots }) => requestRobots.robots)
+	const isPending = useSelector(({ requestRobots }) => requestRobots.isPending)
+	const error = useSelector(({ requestRobots }) => requestRobots.error)
+	
+	useEffect(() => dispatch(setRequestRobots()), [dispatch])
 
+	const onSearchChange = e => dispatch(setSearchField(e.target.value))
 	const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()))
 
 	return isPending
 		? <h1>Loading</h1> 
 		:	(
-			<div className='tc'>
-				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox searchChange={onSearchChange}/>
-				<Scroll>
-					<CardList robots={filteredRobots} />
-				</Scroll>
-			</div>
+			error
+			? <h1>There has been an error, please try again later. </h1>
+			:	(
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange={onSearchChange}/>
+					<Scroll>
+						<CardList robots={filteredRobots} />
+					</Scroll>
+				</div>
+				)
 			)
 }
 
-const mapStateToProps = ({ searchRobots, requestRobots }) => ({
-	searchField: searchRobots.searchField,
-	robots: requestRobots.robots,
-	isPending: requestRobots.isPending,
-	error: requestRobots.error
-})
-
-const mapDispatchToProps = dispatch => ({
-	onSearchChange: e => dispatch(setSearchField(e.target.value)),
-	onRequestRobots: () => dispatch(setRequestRobots())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
