6d8d1a7c8b7d1e86f757b82983b4920795db47e7
Ignacio Iacovino <nacho.iacovino@gmail.com>
2020-01-08 14:47:16 +0100

    Fix folders not showing on git

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import 'tachyons'
 import './index.css'
-import * as serviceWorker from './serviceWorker.js'
+import * as serviceWorker from './serviceWorker'
 import App from './containers/App'
-import { searchRobots, requestRobots } from './redux/reducers.js'
+import { searchRobots, requestRobots } from './redux/reducers'
 
 const logger = createLogger()
 const rootReducer = combineReducers({ searchRobots, requestRobots })
